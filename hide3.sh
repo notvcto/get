8f2ad7d1113f68de88e3452d1f9a83f48c1f307d
notvcto <git@notvc.to>
2026-04-12 21:43:43 -0400

    Update and rename hide2.sh to hide3.sh

--- hide3.sh
+++ hide3.sh
@@ -22,20 +22,16 @@ btrfs subvolume create /mnt/@log
 btrfs subvolume create /mnt/@pkg
 umount /mnt
 
-echo "[*] Mounting root subvolume with zstd compression..."
+echo "[*] Mounting subvolumes..."
 mount -o compress=zstd,subvol=@ /dev/nvme0n1p2 /mnt
-
-echo "[*] Creating mount points..."
 mkdir -p /mnt/{home,var/log,var/cache/pacman/pkg,boot}
-
-echo "[*] Mounting remaining subvolumes and boot..."
 mount -o compress=zstd,subvol=@home /dev/nvme0n1p2 /mnt/home
 mount -o compress=zstd,subvol=@log /dev/nvme0n1p2 /mnt/var/log
 mount -o compress=zstd,subvol=@pkg /dev/nvme0n1p2 /mnt/var/cache/pacman/pkg
 mount /dev/nvme0n1p1 /mnt/boot
 
-echo "[*] Downloading and installing base system (pacstrap)..."
-pacstrap -K /mnt base linux linux-firmware intel-ucode btrfs-progs networkmanager nano sudo git base-devel hyprland kitty polkit mesa vulkan-intel intel-media-driver
+echo "[*] Downloading base system & GRUB (pacstrap)..."
+pacstrap -K /mnt base linux linux-firmware intel-ucode btrfs-progs networkmanager nano sudo git base-devel hyprland kitty polkit mesa vulkan-intel intel-media-driver grub efibootmgr
 
 echo "[*] Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -44,48 +40,33 @@ echo "[*] Chrooting to configure the system..."
 arch-chroot /mnt /bin/bash <<EOF
 set -e
 
-# 1. Locale & Hostname
+# 1. Timezone & Locale
+ln -sf /usr/share/zoneinfo/America/Santo_Domingo /etc/localtime
+hwclock --systohc
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "arch-laptop" > /etc/hostname
 
-# 2. NetworkManager
+# 2. Hostname
+echo "hide-laptop" > /etc/hostname
+
+# 3. NetworkManager
 systemctl enable NetworkManager
 
-# 3. Users and Passwords
-# Setting root password to match just in case
+# 4. Users and Passwords
 echo "root:kalaof34" | chpasswd
-
-# Creating the user 'Hide'
 useradd -m -G wheel -s /bin/bash Hide
 echo "Hide:kalaof34" | chpasswd
 
-# Granting wheel group sudo access without a password prompt for the install phase
+# Granting wheel group sudo access
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
 chmod 440 /etc/sudoers.d/wheel
 
-# 4. Bootloader Setup
-bootctl install
-
-# Creating systemd-boot entry
-cat <<BOOT > /boot/loader/entries/arch.conf
-title   Arch Linux
-linux   /vmlinuz-linux
-initrd  /intel-ucode.img
-initrd  /initramfs-linux.img
-options root=/dev/nvme0n1p2 rw rootfstype=btrfs
-BOOT
-
-# Setting systemd-boot defaults
-cat <<LOADER > /boot/loader/loader.conf
-default arch.conf
-timeout 3
-console-mode max
-editor no
-LOADER
+# 5. GRUB Bootloader Setup
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
 
 EOF
 
-echo "[+] Installation complete! System is fully staged."
-echo "[+] Type 'umount -R /mnt' and then 'reboot'."
+echo "[+] Installation V3 complete! GRUB is staged."
+echo "[+] Type 'umount -R /mnt', and 'reboot'."
